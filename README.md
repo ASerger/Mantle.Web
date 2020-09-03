5a6fb154942290a33b1f84a0c9723f63ce18eea6
serger <aserger1@gmail.com>
2020-09-02 21:25:29 -0400

    initial test and component structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello-world-router
+# Mantle.Web
 
 ## Project setup
 ```
--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -33,29 +33,7 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
-</script>
+<script src="./HelloWorld.ts" lang="ts" />
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style src="./HelloWorld.scss" scoped lang="scss"></style>
